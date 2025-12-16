69c8d3ac0a2a524e603c21e10af08b690fa6f2eb
Vishal524 <vishalkumar524684@gmail.com>
2025-12-16 14:18:55 +0530

    fixed cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:5174',"https://nims-uni-task.vercel.app/"],
+    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:5174',"https://nims-uni-task.vercel.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
